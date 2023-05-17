00c0b63cafc4c0c835fe3336163d5748a2e48566
ahadul1994 <ahadul02@gmail.com>
2023-05-17 19:58:50 +0530

    05 17 by Subhajit

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IonicSlides } from '@ionic/angular';
+import { register } from 'swiper/element/bundle';
 
+register();
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+    autoplay: true,
+    loop: true,
+  };
+
+  swiperModules = [IonicSlides];
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
 
   constructor() { }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommonFooterComponent } from '../components/common-footer/common-footer.component';
 import { IonicModule } from '@ionic/angular';
@@ -15,6 +15,7 @@ import { IonicModule } from '@ionic/angular';
   ],
   exports: [
     CommonFooterComponent
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SharedModule { }
