78d22a9565a7aeb853ef1d023f68897d3685f471
shankha <ahadul02@gmail.com>
2023-11-14 01:05:54 +0530

    14-11-2023

--- src/app/pages/event-details/event-details.page.ts
+++ src/app/pages/event-details/event-details.page.ts
@@ -1,12 +1,14 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { CommonUtils } from 'src/app/services/common-utils/common-utils';
 import { environment } from 'src/environments/environment';
 import profileMenuData from 'src/app/services/profilemenu.json';
+import { IonicSlides } from '@ionic/angular';
+import moment from 'moment';
 
 @Component({
   selector: 'app-event-details',
@@ -30,6 +32,16 @@ export class EventDetailsPage implements OnInit {
 
   userType:any;
 
+  swiperModules = [IonicSlides];
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+    autoplay: true,
+    loop: true,
+  };
+
   constructor(
     changeDetectorRef: ChangeDetectorRef, 
     media: MediaMatcher,
@@ -52,6 +64,7 @@ export class EventDetailsPage implements OnInit {
   }
 
   /* User detasils get start */
+  tutorDetails:any;
   userInfoData(){
     let userObs: Observable<any>;
     userObs = this.authService.userDetails();
@@ -61,6 +74,7 @@ export class EventDetailsPage implements OnInit {
         console.log('userDetails@@', resData);
         if(resData.return_status > 0){
           this.userData = resData.return_data;
+          this.tutorDetails = resData.return_data.user_data;
         }
         
       },
@@ -71,12 +85,13 @@ export class EventDetailsPage implements OnInit {
   /* User detasils get end */
 
   /* getEventView start */
+  tutorAge:any
   getEventView(){
     this.eventViewDataSubscribe = this.http.get(this.eventView_url).subscribe(
       (res:any) => {
         this.eventViewData = res.return_data;
         console.log('eventViewData', this.eventViewData);
-        
+        this.tutorAge = moment().diff(this.eventViewData.tutor_details.dob, 'years');
       },
       errRes => {
       }
